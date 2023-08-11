506cdd1dd1f302275055fa334ea8171555e6e6db
Yuan Li <liyuan.montreal@gmail.com>
2023-08-11 13:10:29 -0400

    version1.2

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 import sqlite3
 from flask import Flask, render_template, request, url_for, flash, redirect
+
+import functools
+
+from flask import (
+    Blueprint, g,  session
+)
+
 from werkzeug.exceptions import abort
+from werkzeug.security import check_password_hash, generate_password_hash
+
 
 ## if write this func before def index(), then will have error
 def get_db_connection():
@@ -8,9 +17,11 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn 
 
-def get_post(post_id):
+def get_post(post_id,check_author=True):
     conn = get_db_connection()
-    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+    post = conn.execute('SELECT p.id, title, content, created, author_id, username'
+                        ' FROM post p JOIN user u ON p.author_id = u.id'
+                        ' WHERE p.id = ?',
                         (post_id,)).fetchone()
     conn.close()
     if post is None:
@@ -25,7 +36,7 @@ app.config['SECRET_KEY'] = 'your secret key'
 @app.route('/')
 def index():
     conn = get_db_connection()
-    posts = conn.execute('SELECT * FROM posts').fetchall()
+    posts = conn.execute('SELECT * FROM post').fetchall()
     conn.close()
     return render_template('index.html', posts=posts)
 
@@ -49,8 +60,10 @@ def create():
             flash('Title is required!')
         else:
             conn = get_db_connection()
-            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
-                         (title, content))
+            conn.execute('INSERT INTO post (title, content, author_id)'
+                        ' VALUES (?, ?, ?)',
+                        (title, content, g.user['id']))
+                    
             conn.commit()
             conn.close()
             return redirect(url_for('index'))
@@ -71,9 +84,9 @@ def edit(id):
             flash('Title is required!')
         else:
             conn = get_db_connection()
-            conn.execute('UPDATE posts SET title = ?, content = ?'
+            conn.execute('UPDATE post SET title = ?, content = ?'
                          ' WHERE id = ?',
-                         (title, content, id))
+                         (title, content, id))                      
             conn.commit()
             conn.close()
             return redirect(url_for('index'))
@@ -86,8 +99,94 @@ def edit(id):
 def delete(id):
     post = get_post(id)
     conn = get_db_connection()
-    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
+    conn.execute('DELETE FROM post WHERE id = ?', (id,))
     conn.commit()
     conn.close()
     flash('"{}" was successfully deleted!'.format(post['title']))
     return redirect(url_for('index'))
+
+#auth
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        conn = get_db_connection()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+
+        if error is None:
+            try:
+                conn.execute(
+                    "INSERT INTO user (username, password) VALUES (?, ?)",
+                    (username, generate_password_hash(password)),
+                )
+                conn.commit()
+            except conn.IntegrityError:
+                error = f"User {username} is already registered."
+            else:
+                return redirect(url_for("auth.login"))
+
+        flash(error)
+
+    return render_template('auth/register.html')
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        conn = get_db_connection()
+        error = None
+        user = conn.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+
+        flash(error)
+
+    return render_template('auth/login.html')
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+    conn = get_db_connection()
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = conn.execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
+
+app.register_blueprint(bp)
